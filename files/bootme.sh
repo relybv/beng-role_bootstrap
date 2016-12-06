90cf26d75e4e0244ed496673648dd3f56bbf9382
Marco Sjerps <msjerps@beeldengeluid.nl>
2016-12-06 11:56:14 +0100

    fix first copy issue

--- files/bootme.sh
+++ files/bootme.sh
@@ -70,7 +70,11 @@ echo "Preparing modules"
 /opt/puppetlabs/puppet/bin/rake spec_prep
 
 # copy to puppet module location
-cp -a /root/role_bootstrap/spec/fixtures/modules/* $MODULEDIR
-
+if [ -d $MODULEDIR ]; then
+cp -a /root/beng_fw/spec/fixtures/modules/* $MODULEDIR
+else
+mkdir $MODULEDIR
+cp -a /root/beng_fw/spec/fixtures/modules/* $MODULEDIR
+fi
 echo "Run puppet apply"
 /usr/local/bin/puppet apply -e "include role_bootstrap"
